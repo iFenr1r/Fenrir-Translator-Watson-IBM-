61fbbc25fdc9a40b8fd85cc0607bc1f8ab22cdfa
iFenr1r <richard.lopes1995@gmail.com>
2017-05-15 13:25:51 -0300

    Adicionada função para identificar idioma digitado

--- translate.py
+++ translate.py
@@ -12,3 +12,22 @@ def tradutor(texto,origem,final):
 
         traduzido = json.dumps(translation, indent=2, ensure_ascii=False)
         return traduzido
+
+def identificar(texto,final):
+        import json
+        from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+        language_translator = LanguageTranslator(
+	username='f5eecea1-3055-419e-96a5-e3732b9183c3',
+	password='y2JSGI4u47l8')
+
+        language = language_translator.identify(texto)
+        ident = json.loads(json.dumps(language, indent=2))
+        origem = ident['languages'][0]['language']
+        
+        translation = language_translator.translate(
+            text=texto,
+            source=origem,
+            target=final)
+
+        traduzido = json.dumps(translation, indent=2, ensure_ascii=False)
+        return traduzido
